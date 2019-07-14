b8bf13ddaccceb2254e02215cc756ebc24706328
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2019-07-14 22:35:43 +0100

    set up of router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'heroes', component: HeroesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
