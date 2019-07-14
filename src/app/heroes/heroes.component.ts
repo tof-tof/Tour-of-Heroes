fdc95fa15b80d5fff89324560d6795ff93af4c18
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2019-07-14 15:10:52 +0100

    displays and styles list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,10 +11,15 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 
   heroes=HEROES;
+  /*
   hero: Hero ={
     id: 1,
     name: 'Windstorm'
-  };
+  };*/
+  selectedHero: Hero;
+  onSelect(hero:Hero){
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
